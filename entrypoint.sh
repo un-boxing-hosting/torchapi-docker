73156421cb0d749cc6e8550f583365c053569938
boxing <boxing@boxinghosting.dev>
2025-10-27 08:47:28 -0700

    Update x11vnc password in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ if [ ! -f /app/.Xauthority ]; then
   runuser -u wine -- touch /app/x11vnc.log /app/.Xauthority
 fi
 runuser -u wine -- bash -c 'Xvfb :99 -screen 0 1024x768x24 -ac -br -auth /app/.Xauthority &'
-runuser -u wine -- bash -c 'x11vnc -display WAIT:99 -forever -autoport 5900 -auth /app/.Xauthority -passwd mypassword -o /app/x11vnc.log -bg &'
+runuser -u wine -- bash -c 'x11vnc -display WAIT:99 -forever -autoport 5900 -auth /app/.Xauthority -passwd BoxingHosting! -o /app/x11vnc.log -bg &'
   
 echo "Waiting 5 seconds for X server to initialize..."
 sleep 5
